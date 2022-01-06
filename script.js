627b96230d7f6d6b7135c35715d02925cbf49f3e
Crispitipina <68116083+Mama-simba@users.noreply.github.com>
2022-01-06 14:01:44 +0100

    capturing user's input & comparing guesses

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let computerGuess
+let userGuesses = [];
 
 function init(){
     computerGuess = Math.floor(Math.random() * 100 + 1); // generates a number between 1-100
-    
     document.getElementById("newGameButton").style.display = "none";
     document.getElementById("gameArea").style.display = "none";
 }
@@ -20,3 +20,24 @@ function easyMode(){
 function hardMode(){
     startGameView()
 }
+
+function compareGuess(){
+    const userGuess = Number(document.getElementById("inputBox").value);
+    userGuesses.push(" " + userGuess); // the numbers in the array get a space after ,
+    document.getElementById("guesses").innerHTML = userGuesses;
+
+    if(userGuess > computerGuess) {
+        document.getElementById("textOutput").innerHTML = "Your guess is too high! ❌";
+        document.getElementById("inputBox").value = ""; //resets input field, so that the guess number disappears once guess has been checked
+
+    } else if (userGuess < computerGuess) {
+        document.getElementById("textOutput").innerHTML = "Your guess is too low! ❌";
+        document.getElementById("inputBox").value = ""; 
+        
+    } else {
+        document.getElementById("textOutput").innerHTML = "Bravo! 👏";
+    }
+}
+
+
+
