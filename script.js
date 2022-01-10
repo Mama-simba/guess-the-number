a5d6d013578285383180f3b60d566c4d3ea4e45c
Crispitipina <68116083+Mama-simba@users.noreply.github.com>
2022-01-10 08:50:28 +0100

    Script done

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let userGuesses = [];
 let attempts = 0;
 let maxGuesses;
 
+
 function init(){
     computerGuess = Math.floor(Math.random() * 100 + 1); // generates a number between 1-100
     document.getElementById("newGameButton").style.display = "none";
@@ -36,9 +37,11 @@ function compareGuess(){
 
     if(attempts < maxGuesses){
         if(userGuess > computerGuess) {
+            if (userGuess < high) high = userGuess;
             document.getElementById("textOutput").innerHTML = "Your guess is too high! ❌";
             document.getElementById("inputBox").value = ""; //resets input field, so that the guess number disappears once guess has been checked
         } else if (userGuess < computerGuess) {
+            if(userGuess > low) low = userGuess;
             document.getElementById("textOutput").innerHTML = "Your guess is too low! ❌";
             document.getElementById("inputBox").value = ""; 
         } else {
@@ -57,6 +60,7 @@ function compareGuess(){
             gameEnded()
         }
     }
+    updateRange()
 }
 
 
@@ -70,3 +74,27 @@ function gameEnded(){
  function newGame(){
      window.location.reload();
  }
+
+
+let low = 1;
+let high = 100;
+
+function updateRange(){
+    const rangeOutput = document.getElementById("rangeOutput"); //updates the text
+    rangeOutput.innerText = `${low} - ${high}`;
+    rangeOutput.style.marginLeft = low + "%";
+    rangeOutput.style.marginRight = 100 - high + "%";
+    rangeOutput.classList.add("flash");
+
+    const lowValue = document.getElementById("low");
+    lowValue.style.flex = low + "%";
+    lowValue.style.background = "#fe5542";
+
+    const space = document.getElementById("space");
+    space.style.flex = high - low + "%";
+    space.style.background = "#83e1d0";
+
+    const highValue = document.getElementById("high");
+    highValue.style.flex = 100 - high + "%";
+    highValue.style.background = "#fe5542";
+}
